f4b762e8cb7ed945a30e2fdfcdddd1331a56becd
Aurian-Nogues <aurian.nogues@gmail.com>
2021-02-06 18:46:33 +0100

    playback working

--- playback/playback.py
+++ playback/playback.py
@@ -1,11 +1,18 @@
 import vlc
 import os
+from time import sleep
 
 #define paths
-video_folder = os.path.abspath(os.path.join('..', 'videos'))
-video_name = 'non_encrrypted.mkv'
+video_folder = os.path.abspath('videos')
+video_name = 'test.mp4'
 videos_path = os.path.abspath(os.path.join(video_folder, video_name))
 
+print(videos_path)
+
 # #create vlc media object
-player = vlc.MediaPlayer('1.mp4')
+player = vlc.MediaPlayer(videos_path)
 player.play()
+sleep(4) #needs time to open vlc
+while player.is_playing():
+    sleep(1)
+    
